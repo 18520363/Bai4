04ebddd2f36d58193ec2f4366255d13ee535749d
18520363 <18520363@gm.uit.edu.vn>
2021-04-14 04:21:52 +0700

    Initial commit

---
+++ Bai4/Bai4.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.Windows.Forms;
+using System.IO;
+namespace Bai4
+{
+    public partial class Bai4 : ServiceBase
+    {
+        public Bai4()
+        {
+            InitializeComponent();
+        }
+        [DllImport("wtsapi32.dll", SetLastError = true)]
+        static extern bool WTSSendMessage(
+            IntPtr hServer,
+            [MarshalAs(UnmanagedType.I4)] int SessionId,
+            String pTitle,
+            [MarshalAs(UnmanagedType.U4)] int TitleLength,
+            String pMessage,
+            [MarshalAs(UnmanagedType.U4)] int MessageLength,
+            [MarshalAs(UnmanagedType.U4)] int Style,
+            [MarshalAs(UnmanagedType.U4)] int Timeout,
+            [MarshalAs(UnmanagedType.U4)] out int pResponse,
+            bool bWait);
+        [DllImport("wtsapi32.dll", SetLastError = true)]
+        static extern int WTSGetActiveConsoleSessionID();
+        public static IntPtr WTS_CURRENT_SERVER_HANDLE = IntPtr.Zero;
+        public static int WTS_CURRENT_SESSION = 68;
+
+        public static void Popup()
+        {
+            bool result = false;
+            String title = "Popup";
+            int tlen = title.Length;
+            String msg = "18520363";
+            int mlen = msg.Length;
+            int resp = 0;
+            result = WTSSendMessage(WTS_CURRENT_SERVER_HANDLE, WTS_CURRENT_SESSION, title, tlen, msg, mlen, 0, 0, out resp, false);
+        }
+        protected override void OnSessionChange(SessionChangeDescription changeDescription)
+        {
+            switch (changeDescription.Reason)
+            {
+                case SessionChangeReason.SessionLock:
+                    LogEntry(string.Format("Locked at {0}", DateTime.Now));
+                    break;
+                case SessionChangeReason.SessionLogoff:
+                    LogEntry(string.Format("Logged Off at {0}", DateTime.Now));
+                    break;
+                case SessionChangeReason.SessionLogon:
+                    LogEntry(string.Format("Logged On at {0}", DateTime.Now));
+                    break;
+                case SessionChangeReason.SessionUnlock:
+                    {
+                        LogEntry(string.Format("Unlocked at {0}", DateTime.Now));
+                        Popup();
+                        break;
+                    }
+                default:
+                    break;
+
+            }
+        }
+
+        protected override void OnStart(string[] args)
+            {
+                LogEntry(string.Format("Service Started at {0}", DateTime.Now));
+                //Popup();
+            }
+
+            protected override void OnStop()
+            {
+                LogEntry(string.Format("Service Stopped at {0}", DateTime.Now));
+            }
+
+            void LogEntry(string message)
+            {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory +
+           "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') +
+           ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to. 
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+        }
+    }
+}
---
+++ Bai4/ProjectInstaller.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Bai4
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstallerBai4 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstallerBai4 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstallerBai4
+            // 
+            this.serviceProcessInstallerBai4.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.serviceProcessInstallerBai4.Password = null;
+            this.serviceProcessInstallerBai4.Username = null;
+            // 
+            // serviceInstallerBai4
+            // 
+            this.serviceInstallerBai4.Description = "Bai4 demo";
+            this.serviceInstallerBai4.DisplayName = "Bai4.Demo";
+            this.serviceInstallerBai4.ServiceName = "Bai4";
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstallerBai4,
+            this.serviceInstallerBai4});
+
+            this.serviceInstallerBai4.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+        }
+
+
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstallerBai4;
+        private System.ServiceProcess.ServiceInstaller serviceInstallerBai4;
+    }
+}
